b73ce368960ca9c395ca962e3fad44be15881344
mofumofuchan <you_oki1@yahoo.co.jp>
2020-11-26 23:56:36 +0900

    mainメソッドを作成

--- lis.rb
+++ lis.rb
@@ -1,22 +1,28 @@
 #!/usr/bin/env ruby
 
-print 'lis.rb> '
+def main
+  print 'lis.rb> '
 
-line = gets
+  line = gets
 
-replaced = line.gsub('(', ' ( ').gsub(')', ' ) ')
-split = replaced.split()
+  replaced = line.gsub('(', ' ( ').gsub(')', ' ) ')
+  split = replaced.split()
 
-op = split[1]
-a = split[2].to_i
-b = split[3].to_i
+  op = split[1]
+  a = split[2].to_i
+  b = split[3].to_i
 
-if op == '+'
-  puts a+b
-elsif op == '-'
-  puts a-b
-elsif op == '*'
-  puts a*b
-elsif op == '/'
-  puts a/b
+  if op == '+'
+    puts a+b
+  elsif op == '-'
+    puts a-b
+  elsif op == '*'
+    puts a*b
+  elsif op == '/'
+    puts a/b
+  end
+end
+
+if __FILE__ == $0
+  main
 end
