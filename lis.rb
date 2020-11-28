faae13f1b651fc3184449cb4fd110914418d8289
mofumofuchan <you_oki1@yahoo.co.jp>
2020-11-28 23:27:29 +0900

    字句解析をメソッドに切り出し

--- lis.rb
+++ lis.rb
@@ -1,13 +1,25 @@
 #!/usr/bin/env ruby
+def read(s)
+  # 文字列からScheme式を読み込む
+  return read_from(tokenize(s))
+end
+
+def tokenize(s)
+  # 文字列をトークンのリストに変換する
+  s.gsub('(', ' ( ').gsub(')', ' ) ').split()
+end
+
+def read_from(tokens)
+  # トークンの列から式を読み込む
+  tokens  # TODO 実装
+end
 
 def main
   print 'lis.rb> '
 
   line = gets
 
-  replaced = line.gsub('(', ' ( ').gsub(')', ' ) ')
-  split = replaced.split()
-
+  split = read(line)
   op = split[1]
   a = split[2].to_i
   b = split[3].to_i
