d880ed35ff4115cfb7766a600e2bb63f3d6c1387
mofumofuchan <you_oki1@yahoo.co.jp>
2021-05-09 03:45:00 +0900

    単体テストを追加

---
+++ test_lis.rb
@@ -0,0 +1,19 @@
+require 'test/unit'
+require './lis.rb'
+
+class TC_Lis < Test::Unit::TestCase
+  def test_atom
+    assert_equal(1, atom('01'))
+    assert_equal(-3.14, atom('-3.14000'))
+    assert_equal('a', atom('a'))
+  end
+
+  def test_read_from
+    assert_equal(['+', 1, 3.14], read_from(['(', '+', '1', '3.14', ')']))
+  end
+
+  def test_tokenize
+    assert_equal(['(', '+', '2', '(', '+', '1', '2', ')', ')'],
+                 tokenize('(+ 2 (+ 1 2))'))
+  end
+end
